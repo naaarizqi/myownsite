f929bef2de42b10493d04ae4ace8767889c45ec6
naaarizqi <70641890+naaarizqi@users.noreply.github.com>
2020-12-08 22:13:15 +0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,13 +1,7 @@
 var n = prompt("Let Me Know Your Name:");
-var c = confirm("Hello " + n + " Welcome to Glow Up With Me Beauty Class");
+var c = confirm("Hello " + n + " Welcome to Glow Up With Me");
 if (c == true) {
-    alert('Hello, Get Ready For Glow Up With Me Beauty Class');
+    alert('Hello, Get Ready For Look At Glow Up With Me Website');
 } else {
     alert('No Problem');
-}
-var c = confirm(n + " Silakan Isi Formulir Pendaftaran");
-if (c == true) {
-    alert('Be Ready:)');
-} else {
-    alert('Enjoy');
 }
